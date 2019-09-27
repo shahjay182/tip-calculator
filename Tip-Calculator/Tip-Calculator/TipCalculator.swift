90b8a37641d0a2b5931074cf2eedba53b9929019
shahjay182 <50597556+shahjay182@users.noreply.github.com>
2019-09-27 10:55:16 -0400

    Add files via upload

---
+++ Tip-Calculator/Tip-Calculator/TipCalculator.swift
@@ -0,0 +1,20 @@
+//
+//  TipCalculator.swift
+//  Tip-Calculator
+//
+//  Created by Jay Shah on 2019-09-27.
+//  Copyright © 2019 Jay Shah. All rights reserved.
+//
+
+import Foundation
+class TipCalculator
+{
+    var amountBeforeTaxt:      Double = 0
+    var tipAmount:             Double = 0
+    var tipPercentage:         Double = 0
+    var totalAmount:           Double = 0
+
+    init(<#parameters#>) {
+        <#statements#>
+    }
+}
---
+++ Tip-Calculator/Tip-Calculator/ViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ViewController.swift
+//  Tip-Calculator
+//
+//  Created by Jay Shah on 2019-09-27.
+//  Copyright © 2019 Jay Shah. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var TotalResultLabel: UILabel!
+    @IBOutlet weak var amountBeforeTextField: UITextField!
+    @IBOutlet weak var tipPercentageLabel: UILabel!
+    @IBOutlet weak var tipPercentageSlider: UISlider!
+    @IBOutlet weak var numberOfPeopleLabel: UILabel!
+    @IBOutlet weak var eachPersonAmountLabel: UILabel!
+    @IBOutlet weak var eachPersonSlider: UISlider!
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    
+    
+    func calculateBill(){
+      
+    }
+
+}
+
